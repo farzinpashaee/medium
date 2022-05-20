db75b9964f48703263db902ead8992706344f9ae
Farzin Pashaee <f.pashaee@yahoo.com>
2022-05-21 03:37:04 +0800

    Update Implementation

--- Spring Boot Web MVC Exception Handling ResponseStatusException/src/main/java/fp/string/ExceptionHandling/controller/StudentRestController.java
+++ Spring Boot Web MVC Exception Handling ResponseStatusException/src/main/java/fp/string/ExceptionHandling/controller/StudentRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,8 +20,19 @@ public class StudentRestController {
     StudentService studentService;
 
     @GetMapping("student/{id}")
-    public ResponseEntity<Student> getUser(@PathVariable(name = "id", required = false) Long id  ) throws InvalidIdException, ServiceDownTimeException {
-        Student student =  studentService.findStudentById(null);
+    public ResponseEntity<Student> getUser(@PathVariable(name = "id", required = false) Long id  )  {
+        Student student;
+        try {
+            student = studentService.findStudentById(null);
+        } catch (InvalidIdException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (ServiceDownTimeException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.SERVICE_UNAVAILABLE, e.getMessage(), e);
+        }
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(student);
