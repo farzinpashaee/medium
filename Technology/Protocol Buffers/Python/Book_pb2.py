2e494fa46a51ee3c5e130e59038e879d2ae4fa00
git <f.pashaee@yahoo.com>
2025-01-20 23:36:42 +0800

    Update proto python example

--- Technology/Protocol Buffers/Python/Book_pb2.py
+++ Technology/Protocol Buffers/Python/Book_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nBook.proto\"\\\n\x04\x42ook\x12\x0f\n\x02id\x18\x01 \x01(\x05H\x00\x88\x01\x01\x12\x12\n\x05title\x18\x02 \x01(\tH\x01\x88\x01\x01\x12\x13\n\x06\x61uthor\x18\x03 \x01(\tH\x02\x88\x01\x01\x42\x05\n\x03_idB\x08\n\x06_titleB\t\n\x07_author\"$\n\x06\x41uthor\x12\x11\n\x04name\x18\x01 \x01(\tH\x00\x88\x01\x01\x42\x07\n\x05_nameB\x14\n\x10\x63om.csl.protobufP\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nBook.proto\"e\n\x04\x42ook\x12\x0f\n\x02id\x18\x01 \x01(\x05H\x00\x88\x01\x01\x12\x12\n\x05title\x18\x02 \x01(\tH\x01\x88\x01\x01\x12\x1c\n\x06\x61uthor\x18\x03 \x01(\x0b\x32\x07.AuthorH\x02\x88\x01\x01\x42\x05\n\x03_idB\x08\n\x06_titleB\t\n\x07_author\"$\n\x06\x41uthor\x12\x11\n\x04name\x18\x01 \x01(\tH\x00\x88\x01\x01\x42\x07\n\x05_nameB\x14\n\x10\x63om.csl.protobufP\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,7 +33,7 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n\020com.csl.protobufP\001'
   _globals['_BOOK']._serialized_start=14
-  _globals['_BOOK']._serialized_end=106
-  _globals['_AUTHOR']._serialized_start=108
-  _globals['_AUTHOR']._serialized_end=144
+  _globals['_BOOK']._serialized_end=115
+  _globals['_AUTHOR']._serialized_start=117
+  _globals['_AUTHOR']._serialized_end=153
 # @@protoc_insertion_point(module_scope)
